718ac0af036141995d5f84503ccb5ccda4e650be
Julila Mpesha <julilampesha@gmail.com>
2025-01-24 23:27:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var studentName = "Julila"
+var studentAge = 28
+
+let isEnrolled = true
+
+console.log(studentName)
+console.log(studentAge)
+console.log(isEnrolled)
+
+let fruits = ['Apples', 'Oranges', 'Bananas'];
+fruits.push('Kiwi');
+
+console.log(`Hello, Reader! My name is ${studentName} and I am ${studentAge} years old. 
+    I am currently taking classes in Humber College: ${isEnrolled}.
+    I enjoy eating the following fruits: ${fruits[0]}, ${fruits[1]}, ${fruits[2]}, and sometimes ${fruits[3]}.`);
+
+
+
+const person = new Object();
+person.age = 28;
+person.name = "Julila";
+
+console.log(`My first name is ${person.name} and my current age is ${person.age}.
+    Below is some variations of math to reach my favourite number or numbers, 47, 4 and 7.`);
+
+console.log(23 + 24);
+console.log(49 - 2);
+console.log(10 * 4);
+console.log(21 / 3);
+
+let x = 4
+let y = 7
+console.log(x < y);
+console.log(6 !== y)
+console.log(y > x)
+console.log(4 === y)
+
+let j = true;
+let m = false;
+
+console.log(j && m);
+console.log(j || m);
+console.log(!m);
